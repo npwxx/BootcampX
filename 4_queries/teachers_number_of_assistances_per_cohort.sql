24b6bdc2ce04eba742222c086145c797c8032250
Nicole Woodcock <nicole.xox@gmail.com>
2020-09-16 01:50:36 +0000

    Find total assistances by teacher and cohort

---
+++ 4_queries/teachers_number_of_assistances_per_cohort.sql
@@ -0,0 +1,8 @@
+SELECT (teachers.name) AS teacher, (cohorts.name) AS cohort, COUNT(*) AS total_assistances
+FROM teachers
+JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
+JOIN students ON assistance_requests.student_id = students.id
+JOIN cohorts ON cohort_id = students.cohort_id
+WHERE cohorts.name = 'JUL02'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teacher;
