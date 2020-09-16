106e795f36540998b68b7ad3239c61f56c4aa33a
Nicole Woodcock <nicole.xox@gmail.com>
2020-09-16 00:21:44 +0000

    Find average assistance time by cohort

--- 4_queries/average_assistance_time.sql
+++ 4_queries/average_assistance_time.sql
@@ -1,2 +1,6 @@
-SELECT avg(completed_at - started_at) AS average_assistance_request_time
-FROM assistance_requests;
+SELECT (cohorts.name) AS name, avg(completed_at - started_at) AS average_assistance_time
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time;
